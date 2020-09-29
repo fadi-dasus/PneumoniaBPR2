76db8428d69d5b8c76eaaf752babce5bb412dd14
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-09-29 17:47:36 +0200

    web service initiation

---
+++ WebService/bachelor/src/main/java/com/bachelor/BachelorApplication.java
@@ -0,0 +1,13 @@
+package com.bachelor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BachelorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BachelorApplication.class, args);
+	}
+
+}
---
+++ WebService/bachelor/src/test/java/com/bachelor/BachelorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bachelor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
