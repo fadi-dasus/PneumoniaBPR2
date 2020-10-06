7fe25626f9fa17a93c9fa27226bf8f470651e943
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-10-06 14:33:18 +0200

    create constants

--- WebService/bachelor/src/main/java/com/bachelor/utility/FoldersPathtUtil.java
+++ WebService/bachelor/src/main/java/com/bachelor/utility/FoldersPathtUtil.java
@@ -5,16 +5,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class FoldersPathtUtil {
 
-	public static String normalImagesFolderPath = "E:\\7 semester\\BP\\implementation\\database\\NORMAL";
-	public static String pneumoniaImagesFolderPath = "E:\\7 semester\\BP\\implementation\\database\\PNEUMONIA";
-	public static String temporaryFolderDestination = "E:\\7 semester\\BP\\implementation\\database\\temp";
-
-	
+	public static final String normalImagesFolderPath = "E:\\7 semester\\BP\\implementation\\database\\NORMAL";
+	public static final String pneumoniaImagesFolderPath = "E:\\7 semester\\BP\\implementation\\database\\PNEUMONIA";
+	public static final String temporaryFolderDestination = "E:\\7 semester\\BP\\implementation\\database\\temp";
 
 	@Override
 	public String toString() {
-		return "normalImagesFolderPath=" + normalImagesFolderPath+ "\npneumoniaImagesFolderPath="
-				+ pneumoniaImagesFolderPath + "\ntemporaryFolderDestination=" + temporaryFolderDestination ;
+		return "normalImagesFolderPath=" + normalImagesFolderPath + "\npneumoniaImagesFolderPath="
+				+ pneumoniaImagesFolderPath + "\ntemporaryFolderDestination=" + temporaryFolderDestination;
 	}
 
 }
