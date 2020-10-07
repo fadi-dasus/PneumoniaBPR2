0bf22e35a210c2bf7be5d287872dcbbfacb4862b
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-10-07 16:04:12 +0200

    initial Message is sent from ActivMQ to Node.js

--- WebService/bachelor/src/main/java/com/bachelor/BachelorApplication.java
+++ WebService/bachelor/src/main/java/com/bachelor/BachelorApplication.java
@@ -1,13 +1,54 @@
- package com.bachelor;
+package com.bachelor;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
 
+@EnableJms
 @SpringBootApplication
 public class BachelorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BachelorApplication.class, args);
+//		SpringApplication.run(BachelorApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(BachelorApplication.class, args);
+		Sender sender = context.getBean(Sender.class);
+		sender.sendMessage("order-queue", "Hello Fadi");
+	}
+
+	@Bean
+	public JmsListenerContainerFactory<?> warehouseFactory(ConnectionFactory factory,
+			DefaultJmsListenerContainerFactoryConfigurer configurer) {
+		DefaultJmsListenerContainerFactory containerFactory = new DefaultJmsListenerContainerFactory();
+		configurer.configure(containerFactory, factory);
+
+		return containerFactory;
+	}
+
+	public ActiveMQConnectionFactory connectionFactory() {
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+		return factory;
+	}
+
+	@Bean
+	public JmsTemplate jmsTemplate() {
+		return new JmsTemplate(connectionFactory());
+	}
+
+	@Bean
+	public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory());
+		return factory;
 	}
 
 }
