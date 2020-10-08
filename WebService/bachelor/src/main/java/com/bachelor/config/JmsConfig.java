c21e2dbe77c415f7d6eaafcb3da09be8c7e03a0b
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-10-08 02:26:03 +0200

    configure the beans through yml

--- WebService/bachelor/src/main/java/com/bachelor/config/JmsConfig.java
+++ WebService/bachelor/src/main/java/com/bachelor/config/JmsConfig.java
@@ -2,13 +2,11 @@ package com.bachelor.config;
 
 import javax.jms.ConnectionFactory;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
@@ -16,37 +14,33 @@ import org.springframework.jms.support.converter.MessageType;
 @EnableJms
 @Configuration
 public class JmsConfig {
+	 @Autowired
+	    private ConnectionFactory connectionFactory;
 	
 	  @Bean
 	    public MessageConverter jacksonJmsMessageConverter(){
+
 	        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
 	        converter.setTargetType(MessageType.TEXT);
 	        converter.setTypeIdPropertyName("_type");
 	        return converter;
 	    }
 	  
+//	  @Bean
+//	    public ActiveMQConnectionFactory connectionFactory(){
+//	        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
+//	        return factory;
+//	    }
 	  @Bean
-	    public ActiveMQConnectionFactory connectionFactory(){
-	        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
-	        return factory;
-	    }
-	    @Bean
-	    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(){
+	    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory( ){
 	        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-	        factory.setConnectionFactory(connectionFactory());
-//	        factory.setConcurrency("1-1");
+	        factory.setConnectionFactory(connectionFactory);
 	        factory.setMessageConverter(jacksonJmsMessageConverter());
+	        //factory.setMessageConverter(xmlMarshallingMessageConverter());
 	        return factory;
 	    }
 
-//	@Bean
-//	public JmsListenerContainerFactory<?> warehouseFactory(ConnectionFactory factory,
-//			DefaultJmsListenerContainerFactoryConfigurer configurer) {
-//		DefaultJmsListenerContainerFactory containerFactory = new DefaultJmsListenerContainerFactory();
-//		configurer.configure(containerFactory, factory);
-//
-//		return containerFactory;
-//	}
+
 
 
 
