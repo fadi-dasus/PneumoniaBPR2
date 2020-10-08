06d975b288bfb89c14e3190345a8b4ad2b588886
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-10-08 03:05:59 +0200

    add transaction manager

--- WebService/bachelor/src/main/java/com/bachelor/config/JmsConfig.java
+++ WebService/bachelor/src/main/java/com/bachelor/config/JmsConfig.java
@@ -1,48 +1,71 @@
 package com.bachelor.config;
 
-import javax.jms.ConnectionFactory;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.connection.JmsTransactionManager;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableJms
+@EnableTransactionManagement
 @Configuration
 public class JmsConfig {
-	 @Autowired
-	    private ConnectionFactory connectionFactory;
-	
-	  @Bean
-	    public MessageConverter jacksonJmsMessageConverter(){
+	private static final Logger LOGGER = LoggerFactory.getLogger(JmsConfig.class);
 
-	        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-	        converter.setTargetType(MessageType.TEXT);
-	        converter.setTypeIdPropertyName("_type");
-	        return converter;
-	    }
-	  
-//	  @Bean
-//	    public ActiveMQConnectionFactory connectionFactory(){
-//	        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", "tcp://localhost:61616");
-//	        return factory;
-//	    }
-	  @Bean
-	    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory( ){
-	        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-	        factory.setConnectionFactory(connectionFactory);
-	        factory.setMessageConverter(jacksonJmsMessageConverter());
-	        //factory.setMessageConverter(xmlMarshallingMessageConverter());
-	        return factory;
-	    }
+	@Value("${spring.activemq.broker-url}")
+	private String brokerUrl;
 
+	@Value("${spring.activemq.user}")
+	private String user;
 
+	@Value("${spring.activemq.password}")
+	private String password;
 
+	@Bean
+	public MessageConverter jacksonJmsMessageConverter() {
 
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setTargetType(MessageType.TEXT);
+		converter.setTypeIdPropertyName("_type");
+		return converter;
+	}
 
+	@Bean
+	public CachingConnectionFactory connectionFactory() {
+
+		CachingConnectionFactory factory = new CachingConnectionFactory(
+				new ActiveMQConnectionFactory(user, password, brokerUrl));
+		factory.setClientId("StoreFront");
+		factory.setSessionCacheSize(100);
+		return factory;
+	}
+
+	@Bean
+	public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+		DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+		factory.setConnectionFactory(connectionFactory());
+		factory.setMessageConverter(jacksonJmsMessageConverter());
+		factory.setTransactionManager(jmsTransactionManager());
+		factory.setErrorHandler(t -> {
+			LOGGER.info("Handling error in listening for messages, error: " + t.getMessage());
+		});
+
+		return factory;
+	}
+
+	@Bean
+	public PlatformTransactionManager jmsTransactionManager() {
+		return new JmsTransactionManager(connectionFactory());
+	}
 
 }
--- WebService/bachelor/src/main/java/com/bachelor/service/ImageJMSService.java
+++ WebService/bachelor/src/main/java/com/bachelor/service/ImageJMSService.java
@@ -3,16 +3,19 @@ package com.bachelor.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bachelor.model.Image;
 
 @Service
+
 public class ImageJMSService {
 private static final String IMAGE_QUEUE = "img";
 
 @Autowired
 private JmsTemplate jmsTemplate;
 
+@Transactional
 public void send(Image image){
 	jmsTemplate.convertAndSend(IMAGE_QUEUE,image);
 }
