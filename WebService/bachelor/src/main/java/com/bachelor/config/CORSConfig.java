cd2ff66c7e44dbeea032bb27a39f607723e24a08
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-11-09 21:52:39 +0100

    remove CORS for local development

---
+++ WebService/bachelor/src/main/java/com/bachelor/config/CORSConfig.java
@@ -0,0 +1,16 @@
+package com.bachelor.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+// remeber to remove it in the production profile 
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**").allowedMethods("*");
+	    }
+	
+
+}
