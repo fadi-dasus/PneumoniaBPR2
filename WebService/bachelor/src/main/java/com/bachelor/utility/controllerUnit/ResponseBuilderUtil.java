015ac64240b4a89043dcada024702ef4010bcf2a
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-11-19 21:14:01 +0100

    refactor submit image respons builder

--- WebService/bachelor/src/main/java/com/bachelor/utility/controllerUnit/ResponseBuilderUtil.java
+++ WebService/bachelor/src/main/java/com/bachelor/utility/controllerUnit/ResponseBuilderUtil.java
@@ -25,70 +25,70 @@ public class ResponseBuilderUtil {
 
 	private static final Logger logger = LogManager.getLogger(ImageController.class);
 
+	public ResponseEntity<?> submitImageResponseBuilder(Image img) {
+		try {
+			return submitImageSuccessfulResponseBuilder(img);
+		} catch (URISyntaxException e) {
+			return serverErrorResponseGenerator(e);
+		}
+	}
+	
 	public ResponseEntity<?> getImageByIdResponseBuilder(Optional<Image> optional) {
 		return optional.map(image -> {
 			try {
-				return ResponseEntity.ok()
-						.eTag(Integer.toString(image.getVersion()))
+				return ResponseEntity.ok().eTag(Integer.toString(image.getVersion()))
 						.location(new URI("/getImage/" + image.getId())).body(image);
 			} catch (URISyntaxException e) {
-				e.printStackTrace();
-				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+				return serverErrorResponseGenerator(e);
 			}
 
 		}).orElse(ResponseEntity.notFound().build());
 
 	}
+	
 
-	public ResponseEntity<?> submitImageResponseBuilder(Image img) {
-
-		logger.info("Creating new image with id: {}, path: {}", img.getId(), img.getPhysicalPath());
+	@Transactional(isolation = Isolation.READ_COMMITTED, rollbackFor = NoSuchElementException.class, value = "jpaTransactionManager")
+	public ResponseEntity<?> updateImageHelper(Image image, Integer ifMatch) {
 		try {
-			// Build a created response
-			return ResponseEntity.created(new URI("/getImage/" + img.getId()))
-					.eTag(Integer.toString(img.getVersion()))
-					.body(img);
-		} catch (URISyntaxException e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-		}
-	}
-	
-	
-	@Transactional(isolation = Isolation.READ_COMMITTED, rollbackFor = NoSuchElementException.class,value ="jpaTransactionManager")
-	public
-	 ResponseEntity<?> updateImageHelper(Image image, Integer ifMatch) {
-		 try {
 
-				Optional<Image> existingImage = imageService.getImageById(image.getId());
-		
-				if (!existingImage.get().getVersion().equals(ifMatch)) {
-					return ResponseEntity.status(HttpStatus.CONFLICT).build();
-				}
-				existingImage.get().setStatus(image.getStatus());
-				Image updated = imageService.update(existingImage.get());
-				
-				try {
-					if (updated != null) {
-						return ResponseEntity.ok().location(new URI("/getImage/" + updated.getId()))
-								.eTag(Integer.toString(updated.getVersion())).body(updated);
-					}
-				} catch (URISyntaxException e) {
-					logger.error(e.getMessage());
-					return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-				}
-			} 
-		 // for the optional object 
-		 catch (NoSuchElementException e) {
-				logger.error(e.getMessage());
+			Optional<Image> existingImage = imageService.getImageById(image.getId());
+
+			if (!existingImage.get().getVersion().equals(ifMatch)) {
+				return ResponseEntity.status(HttpStatus.CONFLICT).build();
 			}
-			
-			 	return ResponseEntity.notFound().build();
+			existingImage.get().setStatus(image.getStatus());
+			Image updated = imageService.update(existingImage.get());
+
+			try {
+				if (updated != null) {
+					return ResponseEntity.ok().location(new URI("/getImage/" + updated.getId()))
+							.eTag(Integer.toString(updated.getVersion())).body(updated);
+				}
+			} catch (URISyntaxException e) {
+				logger.error(e.getMessage());
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+			}
+		}
+		// for the optional object
+		catch (NoSuchElementException e) {
+			logger.error(e.getMessage());
+		}
+
+		return ResponseEntity.notFound().build();
 
 	}
-	
-	
-	
-	
-	
-	
+
+	private ResponseEntity<Image> submitImageSuccessfulResponseBuilder(Image img) throws URISyntaxException {
+		return ResponseEntity.created(new URI("/getImage/" + img.getId())).eTag(Integer.toString(img.getVersion()))
+				.body(img);
+	}
+
+	private ResponseEntity<Object> serverErrorResponseGenerator(URISyntaxException e) {
+		logger.error(e.getMessage());
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+	}
+
+
+
+
 }
